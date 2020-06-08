9b85dee28f645f16874a806db88a661bf1fba747
liu ziyan <212448600@qq.com>
2020-06-08 22:57:07 +0800

    subject一级列表以及pagination

--- mock/server.js
+++ mock/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const Mock = require("mockjs");
-// 创建App应用对象：将来设置后台路由的方法都在上面
-const app = express();
 
+// 创建App应用对象
+const app = express();
 // 随机类
 const Random = Mock.Random;
 // 随机中文标题
 Random.ctitle();
 
-// app.use() 中间件，代表接受所有请求
 // 使用cors 解决跨域
 app.use((req, res, next) => {
   // 设置响应头: 将来作为响应数据返回给客户端的~
@@ -18,51 +17,39 @@ app.use((req, res, next) => {
   next(); // 触发下一个中间件/路由
 });
 
-
-app.get("/admin/edu/subject/:page/:limit", (req, res, next) => {
- 
+// http://47.103.203.152/admin/edu/subject/:page/:limit
+app.get('/admin/edu/subject/:page/:limit',(req,res,next)=>{
+  // 获取params参数
   const {
-    page, // 当前页码
-    limit, // 每页数量
+    page,
+    limit
   } = req.params;
-  // 获取请求参数 query查询字符串
+  // 获取请求参数query
   // const {} = req.query;
-
-  // 模拟数据 http://mockjs.com/examples.html
+  // 模拟数据
   const data = Mock.mock({
-    total: Random.integer(+limit+1, limit * 2), // 以某个的范围取一个随机整数
-    // `items|${limit}` 生成数组，数组长度为limit
-    [`items|${limit}`]: [
+    total:Random.integer(+limit+1,limit*2),
+    [`items|${limit}`]:[
       {
-        // 有个属性 _id 初始化值是 1
-        // 遍历会递增 +1
-        "_id|+1": 1,
-        // @ctitle 会使用上面 Random.ctitle() 来生成随机标题
-        // @ctitle(2,5) 控制长度为2-5
-        title: "@ctitle(2,5)",
-        parentId: 0,
+        "_id|+1":1,
+        title:"@ctitle(2,5)",
+        parentId:0,
       },
     ],
   });
-
-  
-
   // 返回响应
   res.json({
-    code: 20000, // 成功状态码
-    success: true, // 成功
-    data, // 成功的具体数据
-    message: "", // 失败原因
-  }); // 将数据装换json字符串返回响应
-});
+    code: 20000,
+    success: true,
+    data,
+    message:"",
+  })
+})
 
-
-
-// 监听端口号 启动服务
-app.listen(9527, "localhost", (err) => {
-  if (err) {
-    console.log("服务器启动失败", err);
+app.listen(9527,"localhost",(err)=>{
+  if(err){
+    console.log("服务器启动失败",err);
     return;
   }
-  console.log("服务器启动成功~");
-});
+  console.log("服务器启动成功")
+})
--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,13 +1,104 @@
-import React, { Component } from "react";
-// 引入antd组件
-import { Button, Table } from "antd";
-// 引入antd字体图标
-import { PlusOutlined, FormOutlined, DeleteOutlined } from "@ant-design/icons";
+// import React, { Component } from "react";
+// // 引入antd组件
+// import { Button, Table } from "antd";
+// // 引入antd字体图标
+// import { PlusOutlined, FormOutlined, DeleteOutlined } from "@ant-design/icons";
 
-import {reqGetSubjectList} from "@api/edu/subject" 
+// import {reqGetSubjectList} from "@api/edu/subject" 
 
-import "./index.less";
+// import "./index.less";
 
+// export default class Subject extends Component {
+//   state={
+//     subjects:{
+//       total:0,
+//       items:[],
+//     },
+//   };
+//   componentDidMount(){
+//     this.getSubjectList(1,10);
+//   }
+//   getSubjectList = async(page,limit)=>{
+//     const result = await reqGetSubjectList(page,limit);
+//     this.setState({
+//       subjects:result,
+//     })
+//   }
+
+//   render() {
+//     const {subjects} = this.state;
+//     const columns = [
+//       {
+//         // 表头显示的内容
+//         title: "分类名称",
+//         // 当前列要显示data中哪个数据（显示数据的key属性）
+//         // data[dataIndex]
+//         dataIndex: "title",
+//         // 遍历元素需要唯一key属性
+//         key: "title",
+//       },
+//       {
+//         title: "操作",
+//         dataIndex: "",
+//         key: "action",
+//         // 列的宽度
+//         width: 200,
+//         // 默认情况下，渲染的内容是纯文本
+//         // 如果想渲染成其他方案（按钮）需要用render方法指定
+//         render: () => (
+//           <>
+//             <Button type="primary">
+//               <FormOutlined />
+//             </Button>
+//             <Button type="danger" className="subject-btn">
+//               <DeleteOutlined />
+//             </Button>
+//           </>
+//         ),
+//       },
+//     ];
+
+//     return (
+//       <div className="subject">
+//         <Button type="primary" className="subject-btn">
+//           <PlusOutlined />
+//           新建
+//         </Button>
+//         <Table
+//           columns={columns} // 决定列头
+//           expandable={{
+//             // 决定列是否可以展开
+//             // 决定行展开时显示什么内容
+//             expandedRowRender: (record) => (
+//               <p style={{ margin: 0 }}>{record.description}</p>
+//             ),
+//             // 决定行是否可以展开
+//             // 返回值true 就是可以展开
+//             // 返回值false 就是不可以展开
+//             rowExpandable: (record) => record.name !== "Not Expandable",
+//           }}
+//           dataSource={subjects.items} // 决定每一行显示的数据
+//           rowKey="_id"
+//           pagination={{
+//             total:subjects.total,
+//             showQuickJumper: true, // 是否显示快速跳转
+//             showSizeChanger: true, // 是否显示修改每页显示数量
+//             pageSizeOptions: ["5", "10", "15", "20"],
+//             defaultPageSize: 10,
+//             onChange:this.getSubjectList,
+//           }}
+//         />
+//       </div>
+//     );
+//   }
+// }
+import React, { Component } from 'react'
+import {Button,Table} from 'antd';
+import {PlusOutlined ,FormOutlined, DeleteOutlined} from '@ant-design/icons'
+
+import {reqGetSubjectList} from "@api/edu/subject"
+
+import './index.less'
 export default class Subject extends Component {
   state={
     subjects:{
@@ -15,60 +106,52 @@ export default class Subject extends Component {
       items:[],
     },
   };
+
   componentDidMount(){
     this.getSubjectList(1,10);
   }
-  getSubjectList = async(page,limit)=>{
+
+  getSubjectList = async (page,limit)=>{
     const result = await reqGetSubjectList(page,limit);
     this.setState({
       subjects:result,
-    })
+    });
+  };
+
+  handleSizeChange = (page,size)=>{
+    this.getSubjectList(page,size)
   }
 
   render() {
     const {subjects} = this.state;
     const columns = [
-      {
-        // 表头显示的内容
-        title: "分类名称",
-        // 当前列要显示data中哪个数据（显示数据的key属性）
-        // data[dataIndex]
-        dataIndex: "title",
-        // 遍历元素需要唯一key属性
-        key: "title",
+      {  title: '分类名称',
+         dataIndex: 'title', //代表当前列要显示data中哪个数据(显示数据的keyshux)
+         key: 'name'//遍历的每一列都要有唯一的key属性
       },
+      
       {
-        title: "操作",
-        dataIndex: "",
-        key: "action",
-        // 列的宽度
-        width: 200,
-        // 默认情况下，渲染的内容是纯文本
-        // 如果想渲染成其他方案（按钮）需要用render方法指定
-        render: () => (
-          <>
-            <Button type="primary">
-              <FormOutlined />
-            </Button>
-            <Button type="danger" className="subject-btn">
-              <DeleteOutlined />
-            </Button>
-          </>
-        ),
+        title: '操作',
+        dataIndex: '',
+        key: 'action',
+        width:200,
+        // align:"right",
+        render: () => <>
+          <Button type="primary"><FormOutlined/></Button>
+          <Button type="danger" className="subject-btn"><DeleteOutlined/></Button>
+        </>, //默认情况下,渲染的内容是纯文本,如果想渲染成其他方案,需要用render方法指定
       },
     ];
-
+    
     // const data = [
     //   {
     //     key: 1,
-    //     name: "John Brown",
+    //     name: 'John Brown',
     //     age: 32,
-    //     address: "New York No. 1 Lake Park",
-    //     description:
-    //       "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+    //     address: 'New York No. 1 Lake Park',
+    //     description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
     //   },
     // ];
-
     return (
       <div className="subject">
         <Button type="primary" className="subject-btn">
@@ -76,19 +159,14 @@ export default class Subject extends Component {
           新建
         </Button>
         <Table
-          columns={columns} // 决定列头
-          expandable={{
-            // 决定列是否可以展开
+          columns={columns}//决定列头 
+          expandable={{//决定是否可以展开
             // 决定行展开时显示什么内容
-            expandedRowRender: (record) => (
-              <p style={{ margin: 0 }}>{record.description}</p>
-            ),
-            // 决定行是否可以展开
-            // 返回值true 就是可以展开
-            // 返回值false 就是不可以展开
-            rowExpandable: (record) => record.name !== "Not Expandable",
+            expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+            // 具体决定每一行是否可以展开
+            rowExpandable: record => record.name !== 'Not Expandable',
           }}
-          dataSource={subjects.items} // 决定每一行显示的数据
+          dataSource={subjects.items} //决定每一行的数据
           rowKey="_id"
           pagination={{
             total:subjects.total,
@@ -96,10 +174,11 @@ export default class Subject extends Component {
             showSizeChanger: true, // 是否显示修改每页显示数量
             pageSizeOptions: ["5", "10", "15", "20"],
             defaultPageSize: 10,
-            onChange:this.getSubjectList,
+            onChange: this.getSubjectList,
+            onShowSizeChange: this.handleSizeChange
           }}
-        />
+        />,
       </div>
-    );
+    )
   }
 }
