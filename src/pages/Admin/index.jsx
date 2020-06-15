0a4eaff5b1d649bb671a85cb9a3c74a621291fed
liu ziyan <212448600@qq.com>
2020-06-15 23:30:55 +0800

    'feat:饼状图'

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 import Visits from "./components/Visits";
-import Sales from "./components/Sales"
+import Sales from "./components/Sales";
+import Search from "./components/Search";
+import SearchRight from "./components/SearchRight";
 
 export default function Admin() {
-  return (
-    <div>
-      <Visits />
-      <Sales/>
-    </div>
-  );
+	return (
+		<div>
+			<Visits />
+			<Sales />
+			<Search />
+			<SearchRight />
+		</div>
+	);
 }
